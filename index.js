164afbf8c9705fb62d877031cca636ff1df1a5b3
VIKRAM5555 <85481967+VIKRAM5555@users.noreply.github.com>
2023-03-15 15:59:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const uri =
   "mongodb+srv://narashimman54:lakshmi97@cluster0.n63nudw.mongodb.net/?retryWrites=true&w=majority";
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(""));
 
 async function main() {
   const client = new MongoClient(uri);
